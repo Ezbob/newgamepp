1486e2c97401eb2ab6f54aa7f8ecb1608c24b30f
Anders Busch <andersbusch@gmail.com>
2021-12-15 22:22:21 +0100

    file open dialog example

--- src/gui/TileWindow.cc
+++ src/gui/TileWindow.cc
@@ -3,6 +3,7 @@
 #include "raygui.h"
 #include "Constants.hh"
 #include <stdint.h>
+#include "nfd.h"
 
 bool TileWindow::render() {
   if (GuiWindowBox(windowBoundary_, "Tile debugger")) {
@@ -25,7 +26,16 @@ bool TileWindow::render() {
   auto mouseGridPosition = GuiGrid({0, 0, Constants::screenWidth, Constants::screenHeight}, 10.f, 2);
   GuiSetStyle(DEFAULT, LINE_COLOR, oldStyle);
 
-  GuiGroupBox({windowBoundary_.x + 5.f, windowBoundary_.height - 205.f, windowBoundary_.width - 10.f, 200.f}, "Tilesets");
+  auto tileBox = Rectangle{windowBoundary_.x + 5.f, windowBoundary_.height - 205.f, windowBoundary_.width - 10.f, 200.f};
+  GuiGroupBox(tileBox, "Tilesets");
+
+  if (GuiButton({tileBox.x + 10.f, tileBox.y + 10.f, 100.f, 30.f}, "Browse")) {
+    nfdchar_t *path = NULL;
+    nfdresult_t result = NFD_OpenDialog(NULL, NULL, &path);
+    if (result == NFD_OKAY) {
+      TraceLog(LOG_INFO, "Found path %s\n", path);
+    }
+  }
 
   return true;
 }
