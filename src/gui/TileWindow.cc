ebc251e341a49be4c167fbcde58b6e89199515e0
Anders Busch <andersbusch@gmail.com>
2021-12-15 20:34:21 +0100

    starting on tileset editor

--- src/gui/TileWindow.cc
+++ src/gui/TileWindow.cc
@@ -11,9 +11,8 @@ bool TileWindow::render() {
   mousepressed_ = IsMouseButtonPressed(0);
 
   auto gridColorbutton = Rectangle{windowBoundary_.x + 10.f, windowBoundary_.y + 32.f, 100.f, 30.f};
-  if (GuiButton(gridColorbutton, "Grid color")) {
-    showGridColor_ = !showGridColor_;
-  }
+
+  showGridColor_ = GuiToggle(gridColorbutton, "Grid color", showGridColor_);
 
   if (showGridColor_) {
     Rectangle colorWindow = {gridColorbutton.x + 16.f, gridColorbutton.y + 32.f, 200.f, 200.f};
@@ -25,6 +24,8 @@ bool TileWindow::render() {
   GuiSetStyle(DEFAULT, LINE_COLOR, ColorToInt(gridColor_));
   auto mouseGridPosition = GuiGrid({0, 0, Constants::screenWidth, Constants::screenHeight}, 10.f, 2);
   GuiSetStyle(DEFAULT, LINE_COLOR, oldStyle);
+
+  GuiGroupBox({windowBoundary_.x + 5.f, windowBoundary_.height - 205.f, windowBoundary_.width - 10.f, 200.f}, "Tilesets");
 
   return true;
 }
